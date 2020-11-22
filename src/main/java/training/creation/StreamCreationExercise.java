bd3896fc798f56c2dc7bff0f8355d73fee87b9d4
KonstantyFlint <43811569+KonstantyFlint@users.noreply.github.com>
2020-11-22 23:38:46 +0100

    Add files via upload
    ffffff

--- src/main/java/training/creation/StreamCreationExercise.java
+++ src/main/java/training/creation/StreamCreationExercise.java
@@ -8,32 +8,32 @@ public class StreamCreationExercise implements StreamCreator {
 
     @Override
     public IntStream positiveDigitsUsingOf() {
-        return IntStream.empty();
+        return IntStream.of(1,2,3,4,5,6,7,8,9);
     }
 
     @Override
     public IntStream positiveDigitsUsingRange() {
-        return IntStream.empty();
+        return IntStream.range(1,10);
     }
 
     @Override
     public IntStream positiveDigitsUsingIterate() {
-        return IntStream.empty();
+        return IntStream.iterate(1,n->n+1).limit(9);
     }
 
     @Override
-    public IntStream powersOfTwo() {
-        return IntStream.empty();
+    public IntStream powersOfTwo()  {
+        return IntStream.iterate(2,n->n*2).limit(10);
     }
 
     @Override
     public DoubleStream from0Till1WithTenSteps() {
-        return DoubleStream.empty();
+        return DoubleStream.iterate(0,n->(n+1)).map(n->n/10).limit(10);
     }
 
     @Override
     public Stream<String> alphabet() {
-        return Stream.empty();
+        return IntStream.rangeClosed('A','Z').mapToObj((n)->String.format("%c",n));
     }
 
 }
--- src/test/java/training/intermediateoperators/IntermediateOperatorsTest.java
+++ src/test/java/training/intermediateoperators/IntermediateOperatorsTest.java
@@ -56,7 +56,7 @@ public class IntermediateOperatorsTest {
         );
     }
 
-    @Test public void staircaseTest() {
+    @Test public void increasingSawtoothTest() {
         tester(
                 instance,
                 Stream.of(0, 0, 1, 0, 1, 2, 0, 1, 2, 3, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6),
